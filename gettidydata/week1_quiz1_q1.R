01fa0f47d6c521012dc7f2d9e03b147b92e7bcc0
Rick Henderson <rick.henderson.blog@gmail.com>
2016-03-02 23:04:21 -0500

    Completed week1_quiz1_q1.R
    Wrote an R script to answer the question to the first quiz question.

---
+++ gettidydata/week1_quiz1_q1.R
@@ -0,0 +1,32 @@
+# Week 1 Quiz
+# Get Clean Data
+
+# Download the Dataset for the Week 1 Quiz, Q1
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+#download.file(fileURL, ".\\data\\q1.csv")
+
+# Set the date the file was downloaded
+#dateDownloaded <- date()
+# Wed Mar 02 22:22:12 2016
+
+# Read in the file as a data frame
+df <- read.csv("data//q1.csv")
+
+# Narrow the data down to one column / variable (the VAL column for house values)
+houseValues <- df$VAL
+
+# Eliminate NA Values to store only the good data
+goodHouseValues <- houseValues[!is.na(houseValues)]
+
+# Convert the vector of integers into a data frame for subsetting
+# Required to find the # of houses with VAL == 24
+ans <- data.frame(goodHouseValues)
+
+# Subset the data properly to filter just the houses with VAL == 24
+allExpensiveHouses <- ans[ans$goodHouseValues == 24, ]
+
+# Get the number of rows, but its now a vector of integers and not a frame
+q1Answer <- length(allExpensiveHouses)
+
+# Display the results
+q1Answer
