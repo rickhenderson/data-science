d820bfb7554aceb447369b50cfb91e6d61accceb
Rick Henderson <rick.henderson.blog@gmail.com>
2016-03-04 13:38:34 -0500

    Update some files and Finish q4
    q4_readXML.R now outputs the correct results.
    readXLSX.R now shows that it only works if the mode is set to WB for
    saving binary files when you download them as posted on StackOverflow.

--- gettidydata/q4_readXML.R
+++ gettidydata/q4_readXML.R
@@ -3,7 +3,7 @@
 # Entered and modified by Rick Henderson
 # Date Modified: 20160303
 
-library(XML)
+library("XML")
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
 
 # Check to see if the data folder exists, otherwise create it.
@@ -11,17 +11,23 @@ if (!file.exists("data")) {
      dir.create("data")
 }
 
-#download.file(fileURL, ".\\data\\baltimore_menu.xml") # For Windows
+download.file(fileURL, ".\\data\\baltimore_menu.xml") # For Windows
 dateDownloaded <- date()
 message(paste0("The file was downloaded on ", dateDownloaded, "."))
 
 doc <- xmlTreeParse(".\\data\\baltimore_menu.xml", useInternal = TRUE)
+
+# Get the root node
 rootNode <- xmlRoot(doc)
-xmlName(rootNode)
+#xmlName(rootNode)
 
-# Get the zipcodes
-allZipCodes <- xpathSApply(rootNode, "//zipcode", xmlValue)
-quarry <- allZipCodes[allZipCodes=="21213"]
+# Get a single chr vector of all the zipcodes
+zipcodes = xpathSApply(rootNode, "//zipcode", xmlValue)
 
-# How many restaurants have zipcode 21213?
-message(paste0("There were ", length(quarry), " restaurants found with zipcode = 21213."))
+# Convert them to a vector for filtering/subsetting
+zipVector <- zips[zips=="21231"]
+
+# Output the results
+
+# How many restaurants have zipcode 21231?
+message(paste0("There were ", length(zipVector), " restaurants found with zipcode = 21231."))
--- gettidydata/readXLSX.R
+++ gettidydata/readXLSX.R
@@ -3,7 +3,7 @@
 # Entered and modified by Rick Henderson
 # Date Modified: 20160303
 
-#library(xlsx)
+library(xlsx)
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
 
 # Check to see if the data folder exists, otherwise create it.
@@ -11,14 +11,14 @@ if (!file.exists("data")) {
      dir.create("data")
 }
 
-#download.file(fileURL, ".\\data\\q3.xlsx") # For Windows
+#download.file(fileURL, destfile='./data/sample.xlsx', mode='wb')
 dateDownloaded <- date()
 message(paste0("The file was downloaded on ", dateDownloaded, "."))
 
 colIndex <- 7:15
 rowIndex <- 18:23
-dat <- read.xlsx("data\\q3.xlsx", sheetIndex=1,
+dat <- read.xlsx("data\\sample.xlsx", sheetIndex=1,
                         colIndex=colIndex, rowIndex = rowIndex)
 dat
 
-#sum(dat$Zip*dat$Ext,na.rm=T)
+sum(dat$Zip*dat$Ext,na.rm=T)
