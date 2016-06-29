4671145dde50e35a596fa018792c0557041fcb4e
Rick Henderson <rhenderson@wlu.ca>
2016-06-29 16:21:00 -0400

    Add sample a1.R but solution is already somewhere else.

---
+++ 04_Exploratory_Data_Analysis/a1.R
@@ -0,0 +1,5 @@
+# a1.R
+# Household Power Consumption (large dataset - 126MB)
+# Data Source: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+# I have this work somewhere I just have to find it.
