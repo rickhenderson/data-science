6626096a30e12660ac79d5aca9d1dc9ee1de455e
Rick Henderson <rhenderson@wlu.ca>
2016-06-30 23:21:49 -0400

    Add plot1.R and update a1.R

--- 04_Exploratory_Data_Analysis/plot1.R
+++ 04_Exploratory_Data_Analysis/plot1.R
@@ -1,36 +1,19 @@
-# a1.R
+# plot1.R
+# Solution to Assignment 1, Week 1 for Exploratory Data Analysis
+# Author: Rick Henderson
 # Household Power Consumption (large dataset - 126MB)
 # Data Source: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 
-# I have this work somewhere I just have to find it.
-source = "household_power_consumption.txt"
-
 #install.packages("data.table")
-#install.packages("lubridate")
-#install.packages("dplyr")
-
-library(lubridate)   # For working with dates but might not be necessary
 library(data.table)  # For using fread
-library(dplyr)       # For subsetting dates
+
+source = "household_power_consumption.txt"
 
 # Read the data quickly using fread
 df = fread(source, na.strings = "?", verbose = FALSE)
 
-# Subset using the specified dates
-start_date = as.Date("2007-02-01")
-end_date = as.Date("2007-02-02")
-
 # Wrap the data frame as a table data frame to improve printing
 df <- tbl_df(df)
-
-# Converting date / times
-# 1. Paste the date/time together
-# d1 = paste(df$Date[1], df$Time[1])
-# 2. Convert it using strptime
-# d1 = strptime(d1, "%d/%m/%Y %H:%M:%S")
-
-# Getting just the first date
-#df$Date[1]
 
 # Or simply converting all the dates, then do filtering
 df$Date <- as.Date(df$Date,format="%d/%m/%Y")
@@ -40,4 +23,7 @@ jan2007 <- df[df$Date == as.Date("2007-01-01") | df$Date == as.Date("2007-01-02"
 
 # Remove the large dataset from memory
 remove(df)
+
+# Possible charts
+hist(jan2007$Global_active_power)
 
